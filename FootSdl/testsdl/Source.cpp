7b2ced3d6596c5d77930c1de754bd59a2965b336
Gregoire <gregoire.calonne@live.be>
2020-01-20 17:22:51 +0100

    Changed gitIgnore rule

--- FootSdl/testsdl/Source.cpp
+++ FootSdl/testsdl/Source.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
 
+	
+
 	SDL_Window* window = SDL_CreateWindow
 	("An SDL2 window", // window's title
 		100, 100, // coordinates on the screen, in pixels, of the window's upper left corner
